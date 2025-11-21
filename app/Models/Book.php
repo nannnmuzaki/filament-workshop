a28074f2edbc8badcc5ac7ccc364efbcd61bdcad
nannnmuzaki <162042041+nannnmuzaki@users.noreply.github.com>
2025-11-21 20:56:54 +0700

    feat: add book table migration and model

---
+++ app/Models/Book.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Book extends Model
+{
+    protected $fillable = [
+        'book_category_id',
+        'title',
+        'author',
+        'year',
+        'synopsis',
+        'pdf_path',
+        'cover_path',
+        'stock',
+        // 'available_amount', Kalau mau tambah fitur peminjaman buku
+        // 'borrowed_amount'
+    ];
+
+    public function bookCategory(): BelongsTo
+    {
+        return $this->belongsTo(BookCategory::class);
+    }
+}
---
+++ database/migrations/2025_11_21_092015_create_books_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('book_category_id')
+                ->constrained('book_categories')
+                ->cascadeOnDelete();
+            $table->string('title', 255);
+            $table->string('author', 255);
+            $table->string('year', 255);
+            $table->longText('synopsis');
+            $table->string('pdf_path');
+            $table->string('cover_path');
+            $table->unsignedInteger('stock');
+            // $table->unsignedInteger('available_amount'); Kalau mau nambahin fitur request pinjam sekalian
+            // $table->unsignedInteger('borrowed_amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
