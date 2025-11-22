d6b9b7f09842d49e1f7d377abf615c58484ace15
nannnmuzaki <162042041+nannnmuzaki@users.noreply.github.com>
2025-11-22 13:25:16 +0700

    feat: add record title attribute for global search

--- app/Filament/Resources/BookCategories/BookCategoryResource.php
+++ app/Filament/Resources/BookCategories/BookCategoryResource.php
@@ -20,6 +20,8 @@ class BookCategoryResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = Heroicon::OutlinedRectangleStack;
 
+    protected static ?string $recordTitleAttribute = 'name';
+
     public static function form(Schema $schema): Schema
     {
         return BookCategoryForm::configure($schema);
